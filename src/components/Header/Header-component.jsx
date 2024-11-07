e9d8e6c7efa34b41eb630f5907b9aa282410ada5
Shahwaiz24 <shahwaizafzal90@gmail.com>
2024-11-07 17:05:30 +0500

    Completed and Just Params And Github getting Remaining

--- src/components/Header/Header-component.jsx
+++ src/components/Header/Header-component.jsx
@@ -34,8 +34,9 @@ export default function Header() {
                         <ul className="flex flex-col mt-4 font-medium lg:flex-row lg:space-x-8 lg:mt-0">
                             <li>
                                 <NavLink
+                                 to={""}
                                     className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-gray-700" : "text-orange-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Home
@@ -43,8 +44,9 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
+                                to={"/about-us"}
                                     className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-gray-700" : "text-orange-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     About Us
@@ -52,8 +54,9 @@ export default function Header() {
                             </li>
                             <li>
                                 <NavLink
+                                   to={"/contact-us"}
                                     className={({isActive}) =>
-                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-gray-700" : "text-orange-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
+                                        `block py-2 pr-4 pl-3 duration-200 border-b border-gray-100 ${isActive ? "text-orange-700" : "text-gray-700"} hover:bg-gray-50 lg:hover:bg-transparent lg:border-0 hover:text-orange-700 lg:p-0`
                                     }
                                 >
                                     Contact Us
